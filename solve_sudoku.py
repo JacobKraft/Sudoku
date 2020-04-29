c6262cef8956f41d21e19834231dec72ec424ac6
Jacob <jmkraft2@wisc.edu>
2020-04-29 14:51:32 -0500

    finished backtrack algorithm fixed bug in finding empty location

--- solve_sudoku.py
+++ solve_sudoku.py
@@ -1,38 +1,35 @@
+from pprint import pprint
+
 
 def find_empty(arr, loc):
     for x in range(9):
         for y in range(9):
-            if arr[x][x] == 0:
+            if arr[x][y] == 0:
                 loc[0] = x
                 loc[1] = y
                 return True
     return False
 
 
-def check_box(arr, row, col, temp):
-    for x in range(3):
-        for y in range(3):
-            if arr[x+row][y+col] == temp:
-                return False
-    return True
-
-
-def check_col(arr, col, num):
-    for i in range(9):
-        if arr[i][col] == num:
-            return False
-    return True
-
-
-def check_row(arr, row, num):
+def check_valid(arr, row, col, num):
+    # checks row
     for i in range(9):
         if arr[row][i] == num:
             return False
+
+    # checks col
+    for i in range(9):
+        if arr[i][col] == num:
+            return False
+
+    # checks box
+    boxx = row - row % 3
+    boxy = col - col % 3
+    for x in range(3):
+        for y in range(3):
+            if arr[boxx][boxy] == num:
+                return False
     return True
-
-
-def check_valid(arr, row, col, num):
-    return not check_box(arr, row, col, num) and not check_col(arr, col, num) and not check_row(arr, row, num)
 
 
 def solver(arr):
@@ -42,14 +39,15 @@ def solver(arr):
     row = loc[0]
     col = loc[1]
     for num in range(1, 10):
-        check_valid(arr, row, col, num)
+        if check_valid(arr, row, col, num):
+            arr[row][col] = num
+            if solver(arr):
+                return True
+            arr[row][col] = 0
     return False
 
 
 if __name__ == '__main__':
-    # creating a 2D array for the grid
-    grid = [[0 for x in range(9)] for y in range(9)]
-
     # assigning values to the grid
     grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
             [5, 2, 0, 0, 0, 0, 0, 0, 0],
@@ -60,4 +58,8 @@ if __name__ == '__main__':
             [1, 3, 0, 0, 0, 0, 2, 5, 0],
             [0, 0, 0, 0, 0, 0, 0, 7, 4],
             [0, 0, 5, 2, 0, 6, 3, 0, 0]]
-    solver(grid)
+    if solver(grid):
+        pprint(grid)
+    else:
+        print
+        "No solution exists"
